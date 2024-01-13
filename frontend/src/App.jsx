04bb88d8877ec6da108b2bee26c499bc553ed2d7
Misganaw-Berihun <misganawbmb@gmail.com>
2024-01-13 10:12:22 +0300

    feat: add role selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import StaffForm from "./components/StaffForm";
+import TraineeForm from "./components/TraineeForm";
+import { Button } from "@mui/material";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [approvedTrainees, setApprovedTrainees] = useState([]);
+  const [trainees, setTrainees] = useState([]);
+  const [activeRole, setActiveRole] = useState(null);
+
+  const handleTraineeApproval = (trainee) => {
+    setApprovedTrainees((prevTrainees) => [...prevTrainees, trainee]);
+  };
+
+  const handleRoleSelection = (role) => {
+    setActiveRole(role);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{ margin: "100px 400px" }}>
+      <div
+        style={{
+          textAlign: "right",
+          marginBottom: "10px",
+        }}
+      >
+        <Button
+          variant={activeRole === "staff" ? "contained" : "outlined"}
+          color="primary"
+          onClick={() => handleRoleSelection("staff")}
+        >
+          Staff Role
+        </Button>
+        <Button
+          variant={activeRole === "trainee" ? "contained" : "outlined"}
+          color="primary"
+          onClick={() => handleRoleSelection("trainee")}
+        >
+          Trainee Role
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      {activeRole === "staff" && (
+        <StaffForm
+          trainees={trainees}
+          onTraineeApproval={handleTraineeApproval}
+        />
+      )}
+
+      {activeRole === "trainee" && (
+        <>
+          <TraineeForm optedInTrainees={approvedTrainees} />
+          {approvedTrainees.length > 0 && (
+            <div>
+              <h3>Approved Trainees</h3>
+              <ul>
+                {approvedTrainees.map((trainee, index) => (
+                  <li key={index}>{trainee.name}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
